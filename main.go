3ee651ca03b93d9cf5b64a9fbdbc17d63f1d75c9
night-slayer18 <83979018+night-slayer18@users.noreply.github.com>
2024-05-06 12:44:07 +0530

    initial commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	fmt.Println("Hello, Worlds!")
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{"msg": "Hello, World!"})
+	})
+	log.Fatal(app.Listen(":4000"))
+}
